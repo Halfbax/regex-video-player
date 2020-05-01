1c782969100e002f9329303a3d665e114ca83716
Leon Spors <mail@leon-spors.de>
2020-05-01 03:09:52 +0200

    initial configuration view model

---
+++ ViewModel/ConfigurationViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexVideoPlayer.ViewModel
+{
+    class ParsingData
+    {
+        public string pattern { get; set; }
+
+        public int groupId { get; set; }
+
+        public ParsingData data { get; set; }
+    }
+
+    class Show
+    {
+        public string name { get; set; }
+
+        public int Episodes { get; set; }
+
+        public string initialURI { get; set; }
+
+        public ParsingData data { get; set; }
+    }
+
+    class ConfigurationViewModel
+    {
+        public IEnumerable<Show> shows { get; set; }
+    }
+}
